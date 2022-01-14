9aa9ead1d143ceff9f1c897ff8bfe94f53c12e5a
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-14 19:14:56 -0300

    Upload project front

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RoleManager-web-frontend
+
+
+## HOW TO RUN | Port 3000
+```
+npm install
+npm build
+npm start
+```
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div style={{display:"flex",alignItems:"center",justifyContent:"center",minHeight:"100vh"}}>
+      <h1>Hello World, Patrick</h1>
+    </div>
+  )
+}
+
+export default Home
