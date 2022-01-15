3b4b6d57b06f780e630ae3f38afc6b40d61c2095
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-01-14 23:54:09 -0300

    Create navbar and layout component

---
+++ src/components/Navbar/style.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Nav = styled.aside`
+    min-height: 100vh;
+    background-color: var(--violet-blue);
+    width: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem 0;
+
+    ul{
+        width: 100%;
+        list-style: none;
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+    }
+
+    li{
+        cursor: pointer;
+    }
+
+    .Logo{
+        color: #fff;
+    }
+
+    .ContainerMid{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3rem;
+    }
+`;
